9c062285d63e348a9c6ed7571eaf0f8fe43d05db
Leodominoni <95833042+Leodominoni@users.noreply.github.com>
2022-09-27 08:22:01 -0300

    faltou o espaço nenê

--- script_formulario.js
+++ script_formulario.js
@@ -1,6 +1,6 @@
 function enviar(){
     var name = document.getElementById("username");
     if (name.value!=""){
-        alert('Obrigada sr(a) ' + name.value + 'os seus dados foram encaminhados com sucesso');
+        alert('Obrigada sr(a) ' + name.value + ' os seus dados foram encaminhados com sucesso');
     }
 }
